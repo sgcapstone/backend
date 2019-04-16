00ca7ad50a245c47813fb2a58487a19867a9352f
bclar <bc013@email.uark.edu>
2019-04-15 19:33:23 -0500

    added service

--- lib/routes/provider.ts
+++ lib/routes/provider.ts
@@ -4,9 +4,9 @@ import providerController from '../controllers/provider';
 
 const router = express.Router();
 router.get('/', providerController.getAll);
-router.get('/:city', providerController.getByCity);
-router.get('/:providerName', providerController.getByName);
-router.get('/:service', providerController.getByService);
+router.get('/city/:city', providerController.getByCity);
+router.get('/name/:providerName', providerController.getByName);
+router.get('/service/:service', providerController.getByService);
 router.get('/:providerId', providerController.getByProviderId);
 router.post('/', providerController.create);
 router.post('/login', providerController.login);
