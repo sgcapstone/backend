0034b71891ba074996d76c2327be3d9063d708dd
bclar <bc013@email.uark.edu>
2019-04-17 08:22:11 -0500

    msg

--- dist/routes/health.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const health_1 = require("../controllers/health");
-const router = express.Router();
-router.get('/ping', health_1.default.ping);
-exports.default = router;
-//# sourceMappingURL=health.js.map
