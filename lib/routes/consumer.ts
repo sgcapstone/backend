bded7dc668fce4fcd02a4cbcf89a49846a5f3d69
nathanmangrum <46819021+nathanmangrum@users.noreply.github.com>
2019-04-20 15:55:01 -0500

    Update consumer.ts

--- lib/routes/consumer.ts
+++ lib/routes/consumer.ts
@@ -1,13 +1,13 @@
 import * as express from 'express';
 
-import customerController from '../controllers/consumer';
+import consumerController from '../controllers/consumer';
 
 const router = express.Router();
-router.get('/', customerController.getAll);
-router.get('/:customerId', customerController.getByConsumerId);
-router.post('/', customerController.create);
-router.post('/login', customerController.login);
-router.put('/:customerId', customerController.update);
-router.delete('/:customerId', customerController.delete);
+router.get('/', consumerController.getAll);
+router.get('/:customerId', consumerController.getByConsumerId);
+router.post('/', consumerController.create);
+router.post('/login', consumerController.login);
+router.put('/:customerId', consumerController.update);
+router.delete('/:customerId', consumerController.delete);
 
 export default router;
