2d593fb47d670a6f16c90e57d6bdf996f82a710c
bclar <bc013@email.uark.edu>
2019-04-21 19:57:28 -0500

    added count

--- lib/routes/consumer.ts
+++ lib/routes/consumer.ts
@@ -4,8 +4,8 @@ import consumerController from '../controllers/consumer';
 
 const router = express.Router();
 router.get('/', consumerController.getAll);
-router.get('/:customerId', consumerController.getByConsumerId);
 router.get('/count', consumerController.count);
+router.get('/:customerId', consumerController.getByConsumerId);
 router.post('/', consumerController.create);
 router.post('/login', consumerController.login);
 router.put('/:customerId', consumerController.update);
